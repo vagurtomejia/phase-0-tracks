48ccf2dd27143887ac878edf47c1d98f93381df9
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-14 18:40:04 +0200

    add a decript method and test it

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -18,4 +18,29 @@ def encrypt (unencripted_string)
 
 end
 
+
+
+def decrypt (undecrypted_string)
+  #find the length of the string
+  string_length = undecrypted_string.length
+  index = 0
+
+  #create alphabet index
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  #for each letter on the string
+  decrypted_string = ""
+    #find previous letter using the alphabet
+  while index < string_length
+    letter = undecrypted_string[index]
+    index_original = alphabet.index(letter)
+    decrypted_string += alphabet[index_original - 1]
+    index += 1
+  end
+  decrypted_string
+    #replace with the previous letter in the alphabet
+  #print output/result
+end
+
 puts encrypt("zed")
+puts decrypt("afe")
