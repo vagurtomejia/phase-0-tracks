482f2ec57c30494256427163ffd60773d2e9c601
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-14 12:00:47 +0200

    add employee's personal data questions for (release 1)

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+# Gathering the data of the employee
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year_of_birth = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+likes_garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
+needs_insurance = gets.chomp
+
