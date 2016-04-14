2d8db3720a62d3057b1f80231d444c7a9895071f
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-14 15:37:16 +0200

    print the hamster's data (release 3)

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,4 +1,4 @@
-
+#Asks for the hamster data:
 puts "Hamster's name:"
 name = gets.chomp
 
@@ -22,4 +22,32 @@ if age.empty?
   age = nil
 else
   age = age.to_i
+end
+
+#Prints the hamster data:
+puts "**************"
+puts "#{name} data: "
+puts "**************"
+puts "#{name} is a lovely #{fur_color} hamster."
+if age == nil
+  puts "We do not know his age."
+else
+  puts "He is #{age} days old."
+end
+
+if good_candidate
+  puts "He is a good candidate for adoption."
+else
+  puts "Unfortunately, he is not a good candidate for adoption."
+end
+
+case volume_level
+when 0, 1, 2, 3
+  puts "He is silent."
+when 4, 5, 6
+  puts "He is medium noisy."
+when 7, 8, 9, 10
+  puts "He is noisy."
+else
+  puts "Our guess is that he is really silent."
 end
