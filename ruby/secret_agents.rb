c6854f0168eaaeb852c832ccf1fb8aa15187884c
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-14 19:30:04 +0200

    format the code and add comments for more readability

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,61 +1,68 @@
 
+# METHOD DECLARATIONS
+
+#encrypts a string: advances every letter of the string one letter forward
 def encrypt (unencripted_string)
   #find the lenght of the string
   string_lenght = unencripted_string.length
+
+  encripted_string = ""
   index = 0
   #for each letter on the string
-      #replace with the next one into the alphabet
-  encripted_string = ""
   while index < string_lenght
+    #find the position of the letter
     letter = unencripted_string[index]
     if letter == "z"
+      #add "a" to the result
       encripted_string += 'a'
     else
-      encripted_string += unencripted_string[index].next
+      #add the next letter to the result
+      encripted_string += letter.next
     end
     index+=1
   end
   encripted_string
-
 end
 
 
-
+#decrypts a string: reverses the process of the encrypt method
 def decrypt (undecrypted_string)
   #find the length of the string
   string_length = undecrypted_string.length
-  index = 0
 
   #create alphabet index
   alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-  #for each letter on the string
+  index = 0
   decrypted_string = ""
-    #find previous letter using the alphabet
+
+  #for each letter on the string
   while index < string_length
     letter = undecrypted_string[index]
+    #find position in the alphabet
     index_original = alphabet.index(letter)
+    #add previous letter in the alphabet to the result
     decrypted_string += alphabet[index_original - 1]
     index += 1
   end
   decrypted_string
-    #replace with the previous letter in the alphabet
-  #print output/result
 end
+
+# DRIVER CODE
 
 #Asks User if he would like to decrypt or encrypt a password
 puts "Hello! Would you like to decrypt or encrypt a password?"
 encryption_choice = gets.chomp
 
-puts "You chose #{encryption_choice}"
+puts "You chose #{encryption_choice}."
 
 #Asks User for Password
 puts "What is the password?"
 
 password = gets.chomp
-puts "Your password is #{password}"
+puts "Your password is #{password}."
 
-#Encrypts or Decrypts Given Password
+#encrypts or decrypts Given Password
 if encryption_choice == "encrypt"
   result = encrypt(password)
 else
