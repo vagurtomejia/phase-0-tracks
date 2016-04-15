69ef32cd670c8f8d21a7041f7d43dbcbaa6b74c2
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-15 12:57:16 +0200

    add the possibility of processing multiple employees (release 3)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,50 +1,67 @@
+#METHODS DEFINITION
 
-# Asking for employee's data
-valid_input = false;
-while !valid_input
-  puts "What is your name?"
-  employee_name = gets.chomp
-  #would find a match only if the input string contain letters or spaces
-  res = employee_name.match(/^[[:alpha:][:blank:]]+$/)
-  if res != nil
-    valid_input = true
-  else
-    puts "Seriously? Come on! This is not even a name!"
+# asks for employee's data, analyse it (is the employee a vampire?) and return the result
+def vampire_detection
+  valid_input = false;
+  while !valid_input
+    puts "What is your name?"
+    employee_name = gets.chomp
+    #would find a match only if the input string contain letters or spaces
+    res = employee_name.match(/^[[:alpha:][:blank:]]+$/)
+    if res != nil
+      valid_input = true
+    else
+      puts "Seriously? Come on! This is not even a name!"
+    end
   end
+
+  puts "How old are you?"
+  employee_age = gets.chomp.to_i
+
+  puts "What year were you born?"
+  employee_birth_year = gets.chomp.to_i
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  employee_likes_garlic = gets.chomp
+  employee_likes_garlic == "y" ? employee_likes_garlic = true : employee_likes_garlic = false
+
+  puts "Would you like to enroll in the company’s health insurance? (y/n)"
+  employee_needs_insurance = gets.chomp
+  employee_needs_insurance == "y" ? employee_needs_insurance = true : employee_needs_insurance = false
+
+  current_year = 2016
+  computed_age = current_year - employee_birth_year
+  correct_age = computed_age == employee_age ? true : false
+
+  result_of_survey = ""
+  if correct_age && employee_likes_garlic && employee_needs_insurance
+    result_of_survey = "Probably not a vampire."
+  elsif !correct_age && (!employee_likes_garlic || !employee_needs_insurance)
+    result_of_survey = "Probably a vampire."
+  elsif !correct_age && !employee_likes_garlic && !employee_needs_insurance
+    result_of_survey = "Almost certainly a vampire."
+  elsif employee_name == "Drake Cula" || employee_name == "Tu Fang"
+    result_of_survey = "Definitely a vampire."
+  else
+    result_of_survey = "Results inconclusive."
+  end
+
+  result_of_survey
 end
 
-puts "How old are you?"
-employee_age = gets.chomp.to_i
 
-puts "What year were you born?"
-employee_birth_year = gets.chomp.to_i
+#DRIVER CODE
+puts "How many employees do you want to process?"
+nb_employees = gets.chomp.to_i
+nb_current_employee = 1
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-employee_likes_garlic = gets.chomp
-employee_likes_garlic == "y" ? employee_likes_garlic = true : employee_likes_garlic = false
-
-puts "Would you like to enroll in the company’s health insurance? (y/n)"
-employee_needs_insurance = gets.chomp
-employee_needs_insurance == "y" ? employee_needs_insurance = true : employee_needs_insurance = false
-
-current_year = 2016
-computed_age = current_year - employee_birth_year
-correct_age = false
-computed_age == employee_age ? correct_age = true : correct_age = false
-
-result_of_survey = ""
-if correct_age && employee_likes_garlic && employee_needs_insurance
-  result_of_survey = "Probably not a vampire."
-elsif !correct_age && (!employee_likes_garlic || !employee_needs_insurance)
-  result_of_survey = "Probably a vampire."
-elsif !correct_age && !employee_likes_garlic && !employee_needs_insurance
-  result_of_survey = "Almost certainly a vampire."
-elsif employee_name == "Drake Cula" || employee_name == "Tu Fang"
-  result_of_survey = "Definitely a vampire."
-else
-  result_of_survey = "Results inconclusive."
+while nb_current_employee <= nb_employees
+  puts "Vampire-detection survey for the employee number #{nb_current_employee}"
+  puts "******************************************************"
+  detection_result = vampire_detection
+  puts "Our vampire-detection program indicates the following result for this employee: #{detection_result}\n\n"
+  nb_current_employee += 1
 end
-
-puts result_of_survey
+puts "Thank you for using our vampire-detection program!"
 
 
