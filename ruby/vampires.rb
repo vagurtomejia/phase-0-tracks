53fd435c0a1ff25b4e47767d3a28311667e37366
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-15 11:02:43 +0200

    rename variables for a better readability

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,9 +3,9 @@
 valid_input = false;
 while !valid_input
   puts "What is your name?"
-  name = gets.chomp
+  employee_name = gets.chomp
   #would find a match only if the input string contain letters or spaces
-  res = name.match(/^[[:alpha:][:blank:]]+$/)
+  res = employee_name.match(/^[[:alpha:][:blank:]]+$/)
   if res != nil
     valid_input = true
   else
@@ -14,17 +14,17 @@ while !valid_input
 end
 
 puts "How old are you?"
-age = gets.chomp.to_i
+employee_age = gets.chomp.to_i
 
 puts "What year were you born?"
-year_of_birth = gets.chomp.to_i
+employee_birth = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-likes_garlic = gets.chomp
-likes_garlic == "y" ? likes_garlic = true : likes_garlic = false
+employee_likes_garlic = gets.chomp
+employee_likes_garlic == "y" ? employee_likes_garlic = true : employee_likes_garlic = false
 
 puts "Would you like to enroll in the company’s health insurance? (y/n)"
-needs_insurance = gets.chomp
-needs_insurance == "y" ? needs_insurance = true : needs_insurance = false
+employee_needs_insurance = gets.chomp
+employee_needs_insurance == "y" ? employee_needs_insurance = true : employee_needs_insurance = false
 
 
