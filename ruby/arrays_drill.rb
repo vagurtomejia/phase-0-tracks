77f76fc08191a6a9e39158c849598a902e29d1d3
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-20 20:23:31 +0200

    add a method that takes an array and an item as parameters, and returns the array with the item added.

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -7,9 +7,15 @@ def build_diversity_names(first_name, middle_name, last_name)
   diversity_name = [first_name, middle_name, last_name]
 end
 
+#method that takes an array and an item as parameters, and returns the array with the item added.
+def add_to_array(array, element)
+  array << element
+end
+
 #DRIVER CODE
 
 # Initialize an empty array and store it in a variable (you can choose the name). Print the variable using p.
+
 dwarfs = Array.new #or dwarfs = []
 p dwarfs
 # Add five items to your array. Print the array.
@@ -36,12 +42,23 @@ female_protagonist = ["Cinderella", "Rapunzel", "Belle", "Pocahontas", "Jasmine"
 diversity_couples = dwarfs + female_protagonist
 p diversity_couples
 
-puts "\nResult for the call of build_diversity_names(\"Grumpy\", \"Pocahontas\", \"White\")="
+puts "\nResult for the call of build_diversity_names(\"Grumpy\", \"Pocahontas\", \"White\") :"
 p build_diversity_names("Grumpy", "Pocahontas", "White")
 
-puts "Result for the call of build_diversity_names(1, \"two\", nil) ="
+puts "Result for the call of build_diversity_names(1, \"two\", nil) :"
 puts "(... What? Integers and strings should be able to marry each other in a perfect diversity-respectful world :-) )"
 p build_diversity_names(1, "two", nil)
 
+puts "\nResult for the call of add_to_array([], \"a\") :"
+p add_to_array([], "a")
+
+puts "Result for the call of add_to_array([\"a\", \"b\", \"c\", 1, 2], 3) :"
+p add_to_array(["a", "b", "c", 1, 2], 3)
+
+puts "Result for the call of add_to_array([\"Cinderella\", \"Rapunzel\", \"Belle\", \"Pocahontas\"], \"Grumpy\") :"
+p add_to_array(["Cinderella", "Rapunzel", "Belle", "Pocahontas"], "Grumpy")
+
+puts "Result for the call of add_to_array([\"The\"], \"End\") :"
+p add_to_array(["The"], "End")
 
 
