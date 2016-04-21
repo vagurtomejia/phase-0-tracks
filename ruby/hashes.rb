357608545d0bcf73ffa3996d8b83c88777dcdd7e
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-21 18:25:48 +0200

    complete pseudocoding (Release 3)

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,24 +1,40 @@
 
+# create an Hash named client_data
+
 # ask for the client's name
-# retrieve the inouted name
-# store it with the label "name"
+# retrieve the inputed name as a String
+# store it in with the label "name"
 
 # ask for the client's age
-# retrieve the inputed age
+# retrieve the inputed age as an Integer
 # store it with the label "age"
 
 # ask for the client's number of children
-# retrieve the inputed number of children
+# retrieve the inputed number of children as an Integer
 # store it with the label "nb_children"
 
 # ask for the client's decor theme
-# retrieve the inputed decor theme
+# retrieve the inputed decor theme as a String
 # store it with the label "decor_theme"
 
 # ask for the client's favorite color
-# retrieve the inputed favorite color
+# retrieve the inputed favorite color as a String
 # store it with the label "favorite_color"
 
 # ask if the client's wants to apply the feng shui principles in the decor
-# retrieve the inputed feng shui decor preferences
+# retrieve the inputed feng shui decor preferences as a Boolean
 # store it with the label "feng_shui"
+
+# Print client_data back out to the screen
+
+# As the user if he/she wants to update some data:
+# - Type "none" if the no data need to be update
+# - Type the corresponding key if the data need to be updates
+#   -display the keys
+
+# if the user response is "none", do not do anything
+# otherwise,
+#   -ask the user to input a value
+#   -store the value at the corresponding key
+
+# Print client_data back out to the screen
