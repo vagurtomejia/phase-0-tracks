0e6f3de68cab7e7af22e0d3bdca545b247d79b54
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-21 18:09:13 +0200

    pseudocode a program that asks for and stores an interior designer client data in hashes.rb (release 3)

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+
+# ask for the client's name
+# retrieve the inouted name
+# store it with the label "name"
+
+# ask for the client's age
+# retrieve the inputed age
+# store it with the label "age"
+
+# ask for the client's number of children
+# retrieve the inputed number of children
+# store it with the label "nb_children"
+
+# ask for the client's decor theme
+# retrieve the inputed decor theme
+# store it with the label "decor_theme"
+
+# ask for the client's favorite color
+# retrieve the inputed favorite color
+# store it with the label "favorite_color"
+
+# ask if the client's wants to apply the feng shui principles in the decor
+# retrieve the inputed feng shui decor preferences
+# store it with the label "feng_shui"
