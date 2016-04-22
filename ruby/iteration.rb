2bd233ab713ecc81b9354c2dd07079e0f9c00276
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-22 13:32:36 +0200

    Add hash and array initialization in iteration.rb file (Release 1, step 1)

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,5 +1,5 @@
 
-
+#Release 0
 # say_magic_word - method that takes a block and prints a super magic word
 # print out a status message before and after running the block
 def say_magic_word
@@ -10,3 +10,20 @@ def say_magic_word
 end
 
 say_magic_word { |word| puts "The word is #{word}!"}
+
+
+#Release 1
+meaningful_words = {"Mary Poppins" => "supercalifragilisticexpialidocious", "Cinderella" => "Bibbidi-Bobbidi-Boo", "Jack and the Beanstalk" => "Fee-fi-fo-fum!", "Harry Potter" => "Wingardium Leviosa"}
+
+meaningful_words_authors = ["Mary Poppins", "Fairy Godmother", "Blunderbore", "Hermione Granger"]
+
+
+p meaningful_words
+p meaningful_words_authors
+#p meaningful_words["Mary Poppins"]
+
+
+# supercalifragilisticexpialidocious - Mary Poppins
+# Hermione Granger Wingardium Leviosa
+# Bibbidi-Bobbidi-Boo Fairy Godmother - Cinderella
+# Fee-fi-fo-fum!, -Jack and the Beanstalk - Blunderbore.
