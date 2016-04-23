bdeb6a0f96ac77e2682d32a25a2364607a196bc9
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-23 12:22:02 +0200

    print and access elements in Ecuador (Release 2)

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,67 +1,48 @@
 
-Ecuador = {
+ecuador = {
+  official_name: "República del Ecuador",
   capital: "Quito",
   languages: ["Spanish", "Kichwa", "Shuar"],
   government: "Unitary presidential constitutional republic",
   area: [283560, "km2"],
+  location: "South America",
   main_cities: {
     quito: {
       full_name: "San Francisco de Quito",
       population: 2671191,
       elevation: [2850, "m"],
-      province:  "Pichincha"
+      province:  "Pichincha",
+      popular_sayings: [
+        "¡Ele así han de venirbs!",
+        "Que cague de risa",
+        "A que hora te vas vos ve!!!",
+        "Ele, Mijita",
+        "No seas malito",
+        "Te paso viendo",
+        "Harázme el favorcito que te pedí...",
+        "Nofff",
+        "Sifff",
+        "No frieguesfff",
+        "Maesstrroo!!!....",
+        "Profeshhor!!"
+        ]
     },
     guayaquil: {
       full_name: "Santiago de Guayaquil",
       population: 3500000,
       elevation: [4, "m"],
-      province:  "Guayas"
+      province:  "Guayas",
+      popular_sayings: []
     },
     cuenca: {
       full_name: "Santa Ana de los cuatro ríos de Cuenca",
       population: 400000,
       elevation: [2560, "m"],
-      province:  "Azuay"
+      province:  "Azuay",
+      popular_sayings: []
     }
   }
 }
 
-p Ecuador
-
-
-
-# Quito
-# Poet Jorge Carrera Andrade (b. 1902, Quito – d. 1978, Quito)
-# Novelist Jorge Icaza (b. 1906, Quito - d. 1978, Quito)
-# Film Director Sebastián Cordero (b. 1972, Quito)
-# Painter Oswaldo Guayasamín (b. 1919, Quito - d. 1999, Baltimore U.S.)
-# Professional Footballer Christian Benitez (b. May 1, 1986, Quito – d. July 29, 2013, Doha, Qatar)
-# Catholic Saint Mariana de Jesús de Paredes (b.1618,Quito - d. 1645, Quito)
-# Scientist Eugenio Espejo (b. February 21, 1747 - D. December, 1795 )
-
-# Guayaquil
-# Fernanda Cornejo, Miss Ecuador International 2011, Miss International 2011
-# Olga Álava, Miss Ecuador Earth 2011, Miss Earth 2011
-# Sir Frederick Ashton, British choreographer and dancer
-# Novelist Demetrio Aguilera Malta
-# Animator Mike Judge
-# Poets José Joaquín de Olmedo,[8] Karina Galvez[9] and Adalberto Ortiz
-# Writer/historian Jenny Estrada
-# color commentator for the WWE Spanish team and former professional wrestler Hugo Savinovich
-# Scholar Benjamín Urrutia
-# Former world's oldest person María Capovilla
-# Archeologist Presley Norton Yoder
-# Violinist Alex Jimbo Viteri
-# Violinist Jorge Saade
-# Writer Pedro Jorge Vera
-# Actor Albert Paulsen
-# Operatic soprano Beatriz Parra Durango
-# rapper Gerardo Mejía, made famous during the early 90s for his song "Rico Suave"
-# Tennis player Pancho Segura who in 1950 and 1952 was the world's Co-No. 1 player
-# Tennis player Andrés Gómez, who won the ATP Championship of Roland Garros in Paris, France in 1990
-# Swimmer Jorge Delgado 4th in the 1972 Munich Olympic Games, multiple times South American champion (19), Gold Medal 1975 Panamerican champion.
-# Striker Felipe Caicedo, who is currently playing for RCD Espanyol.
-# Striker Joao Plata, who is currently playing for Real Salt Lake.
-# Carolina Aguirre, Miss Universe 2012 Candidate
-# Prima ballerina and choreographer Noralma Vera Arrata
-# Writer and feminist Rosa Borja de Ycaza
+puts "Ecuador, officially #{ecuador[:official_name]} is a beautiful and wonderful country of #{ecuador[:location]}. Its capital city is #{ecuador[:capital]}, formally #{ecuador[:main_cities][:quito][:full_name]}."
+puts "#{ecuador[:capital]} has an elevation of #{ecuador[:main_cities][:quito][:elevation][0]} #{ecuador[:main_cities][:quito][:elevation][1]} and a population of  #{ecuador[:main_cities][:quito][:population]} people. One of the most popular sayings in #{ecuador[:capital]} is \'#{ecuador[:main_cities][:quito][:popular_sayings].sample}\'."
