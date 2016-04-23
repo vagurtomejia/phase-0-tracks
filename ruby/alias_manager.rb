3b8935bc9f20920d10e6a0f5a413e24ace73f897
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-23 14:13:57 +0200

    Add an encode_name method that change the vowels of a name to the next vowel in 'aeiou', and all of the consonants to the next consonant in the alphabet (Release 0)

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,12 @@
 
-#generate_alias method: takes a spy's real name  and creates and return a fake name
-#"Felicia Torres" will return "Vussit Gimodoe"
+#METHOD DECLARATIONS
 
-# downcase the full name
-# split full name into first_name and last_name
-# for both names:
+#encode_name method: change the vowels to the next vowel in 'aeiou', and all of the consonants to the next consonant in the alphabet.
+#"Felicia" will return "Gimodoe"
+def encode_name(name)
+  # for both names:
+  # downcase the name
+#  real_name.downcase!
 #   - convert them into arrays
 #   - iterate into all the letters (elements on the array):
 #     -if the letter is a vowel (a, e, i, o, or u)
@@ -14,6 +16,29 @@
 #     -otherwise,
 #       change the letter into the next consonant in the alphabet
 #       (do not forget to handle side effects)
-# capitalize the first and last name
-# join last and first name (swapping them)
-# return alias_name
+# capitalize the name
+
+#return encoded_name
+end
+
+#generate_alias method: takes a spy's real name  and creates and return a fake name
+#"Felicia Torres" will return "Vussit Gimodoe"
+def generate_alias(real_name)
+
+  # split full name into first_name and last_name
+  names = real_name.split(" ")
+  first_name = names.first
+  last_name = names.last
+
+  new_first_name = encode_name(first_name)
+  new_last_name = encode_name(last_name)
+
+  # join last and first name (swapping them)
+
+  alias_name = real_name #borrar!
+  return alias_name
+end
+
+#DRIVER CODE
+
+p generate_alias("Felicia Torres")
