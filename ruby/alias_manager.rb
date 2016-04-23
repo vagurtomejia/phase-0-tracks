8459d0eea76f1ebb537ce8812c8e404f460f337a
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-23 17:27:12 +0200

    Update generate_alias and encode_name methods for full handle of the alias generation (end Release 0)

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,21 +4,28 @@
 #encode_name method: change the vowels to the next vowel in 'aeiou', and all of the consonants to the next consonant in the alphabet.
 #"Felicia" will return "Gimodoe"
 def encode_name(name)
-  # for both names:
-  # downcase the name
-#  real_name.downcase!
-#   - convert them into arrays
-#   - iterate into all the letters (elements on the array):
-#     -if the letter is a vowel (a, e, i, o, or u)
-#        -search its index index in 'aeiou'
-#        -if the index is >= 'aeiou'.lenght -1, index = 0
-#        -otherwise, change the letter to the corresponding letter with index + 1 in 'aeiou'
-#     -otherwise,
-#       change the letter into the next consonant in the alphabet
-#       (do not forget to handle side effects)
-# capitalize the name
-
-#return encoded_name
+    #downcase the name
+    name.downcase!
+    #convert the name into array
+    name_letters = name.split('')
+    #name_letters.select { |l| l =~ /[aeiou]/ }  #=> ["a", "e"]
+    vowels = ['a','e','i','o','u']
+    consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+    name_letters.map! do |l|
+      index = vowels.find_index(l)
+      if index != nil
+        #side effect management
+        new_index = index >=  vowels.count - 1 ? 0 : index + 1
+        vowels[new_index]
+      else
+        index = consonants.find_index(l)
+        #side effect management
+        new_index = index >=  consonants.count - 1 ? 0 : index + 1
+        consonants[new_index]
+      end
+    end
+    encoded_name = name_letters.join('')
+    encoded_name.capitalize
 end
 
 #generate_alias method: takes a spy's real name  and creates and return a fake name
@@ -34,8 +41,7 @@ def generate_alias(real_name)
   new_last_name = encode_name(last_name)
 
   # join last and first name (swapping them)
-
-  alias_name = real_name #borrar!
+  alias_name = new_last_name + ' ' + new_first_name
   return alias_name
 end
 
