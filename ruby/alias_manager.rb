d4b6dbf7a0cd1163f5b4047faab82b6d18babefa
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-23 13:30:57 +0200

    update generate_alias pseudocode to handle upcase and side effects (Release 0)

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,19 @@
 
+#generate_alias method: takes a spy's real name  and creates and return a fake name
+#"Felicia Torres" will return "Vussit Gimodoe"
 
+# downcase the full name
 # split full name into first_name and last_name
 # for both names:
 #   - convert them into arrays
 #   - iterate into all the letters (elements on the array):
-#     -if the letter is a vowel (a, e, i, o, or u) :
-#       change it to the next vowel in 'aeiou'
+#     -if the letter is a vowel (a, e, i, o, or u)
+#        -search its index index in 'aeiou'
+#        -if the index is >= 'aeiou'.lenght -1, index = 0
+#        -otherwise, change the letter to the corresponding letter with index + 1 in 'aeiou'
 #     -otherwise,
 #       change the letter into the next consonant in the alphabet
-# swap the first and last name
-# return the new full name
+#       (do not forget to handle side effects)
+# capitalize the first and last name
+# join last and first name (swapping them)
+# return alias_name
