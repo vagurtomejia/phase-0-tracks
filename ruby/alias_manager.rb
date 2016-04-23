a3edeb2f524c2562423deb055bee67c6f6fddbfe
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-23 13:00:18 +0200

    add pseudocode for the generate_alias method (Release 0)

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+
+
+# split full name into first_name and last_name
+# for both names:
+#   - convert them into arrays
+#   - iterate into all the letters (elements on the array):
+#     -if the letter is a vowel (a, e, i, o, or u) :
+#       change it to the next vowel in 'aeiou'
+#     -otherwise,
+#       change the letter into the next consonant in the alphabet
+# swap the first and last name
+# return the new full name
