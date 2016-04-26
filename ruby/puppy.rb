ff9efa6b4e4022d5051f5d592ae00e0ec267dd87
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-26 18:46:23 +0200

    Add comments with characteristics and behavior for a Puppy class (Release 0)

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Name: varies
+# Color: varies
+# Friendly: yes
+# Name of the master: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Run
