f88fe585ece9a5da8ddd89ed213fe1ccfbd9a5aa
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-27 17:01:42 +0200

    Add speak, roll_over, dog_years and pee to Puppy class and call them (Release 0)

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,7 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    number.times { puts "Woof!" }
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    human_years * 11
+  end
+
+  def pee(place)
+    puts "*...peeing on #{place}*"
+  end
+
 end
 
 woufwouf = Puppy.new
 woufwouf.fetch("Buzz Lightyear")
+woufwouf.speak(4)
+woufwouf.roll_over
+p woufwouf.dog_years(2)
+woufwouf.pee("the Christmas tree")
