2b4cc458af4511c2dfe1a02a330dfa7b93616725
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-27 20:02:01 +0200

    Add some Santa instances initializations (Release 1)

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,16 +1,33 @@
 
 class Santa
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    reindeer_ranking =["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
-  def initialize
-    puts "Initializing Santa instance ..."
-  end
 end
 
-my_santa = Santa.new
-my_santa.speak
-my_santa.eat_milk_and_cookies("snickerdoodle")
+# my_santa = Santa.new
+# my_santa.speak
+# my_santa.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+santas.each do |santa|
+  puts "Santa #{} is .."
+  santa.speak
+end
+
