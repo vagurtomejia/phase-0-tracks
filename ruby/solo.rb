d3ea26917f62171e889e3e7d7b1db41a2c1a4b3e
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-04-28 12:16:32 +0200

    Design a fortune cookie class (Release 0)

---
+++ ruby/solo.rb
@@ -0,0 +1,10 @@
+#design of the fortune cookie class:
+  # attributes:
+  # target age : integer
+  # target language : string
+  # sentence : string
+  # methods:
+  # prepare_batter (takes preferences : gluten-free, vegan)
+  # bake
+  # tell_fortune
+
