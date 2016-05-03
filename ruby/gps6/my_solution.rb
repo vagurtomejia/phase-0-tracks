4e5fb0b6af11ee9e8a1de09125314e6bcdd4066a
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-03 17:37:30 +0200

    Add reflection to my_solution.rb file (GPS 3.1, Release 9)

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -70,3 +70,32 @@ end
 
 #=======================================================================
 # Reflection Section
+# ***What are the differences between the two different hash syntaxes shown in the state_data file?
+# => One uses symbols as Hash keys, while the other one uses strings as Hash keys. The main differences between them are:
+
+# 1.- Using symbols saves time when doing comparisons:
+# If the Hash key is a string, Ruby needs to evaluate the string and look at it's contents (and compute a hash function on that) and compare the result against the (hashed) values of the keys which are already stored in the Hash.
+# This is much faster using symbols. If the Hash key is a symbol, it's implicit that it's immutable, so Ruby can basically just do a comparison of the (hash function of the) object-id against the (hashed) object-ids of keys which are already stored in the Hash.
+
+# 2.- Using symbols saves memory, because they are only stored once.
+
+# 3.- Using symbols is a memory leak because they can never be garbage collected. So they have to be carefully used.
+
+# ***What does require_relative do? How is it different from require?
+# => require_relative allows to load a file that is relative to the file containing the require_relative statement.
+# require requires a file relative to the current directory.
+# When using require, the argument will be searched for via the load paths, while require_relative essentially uses the absolute path and is therefore more speed
+
+# ***What are some ways to iterate through a hash?
+# Hash#each
+# ie: hash.each { | key, value| puts "#{key}: #{value}" }
+# Hash#each_key
+# ie: hash.each_key { | key | puts key }
+# Hash#map
+# ie: new_hash = hash.map { | key, value | value = "deleted!" }
+
+# ***When refactoring virus_effects, what stood out to you about the variables, if anything?
+# =>I did not knew about the existence of constant variables in Ruby before this challenge. It was a totaly new thing for me.
+
+# What concept did you most solidify in this challenge?
+# =>The Hash keys behavior using symbols (see above)
