702b04e49532e7bcc49da174c8b4b6d6fc6defa3
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-06 11:59:40 +0200

    Replace Random.rand by rand and Array#delete_if by Array£delete

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -22,16 +22,16 @@ class Santa
     @age += 1
   end
   def get_mad_at(reindeer_name)
-    @reindeer_ranking = @reindeer_ranking.delete_if { |reindeer| reindeer_name == reindeer }.push(reindeer_name)
+    removed_reindeer = @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking.push(removed_reindeer)
   end
 end
 
-
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-1000.times do |i|
+1.times do |i|
   santa =  Santa.new(example_genders.sample, example_ethnicities.sample)
-  santa.age = Random.rand(140)
+  santa.age = rand(140)
   puts "Santa number #{i + 1} is #{santa.age} years old, #{santa.gender}, #{santa.ethnicity} ."
 end
 puts "\nLook forward to many enjoyable encounters and interaction at this year's SantaCon!"
