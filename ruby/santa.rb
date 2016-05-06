1b8e538b7bd850573d3eadaf5646bbda5f0a68ab
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-06 11:47:00 +0200

    Add line spaces for more readability of the code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -10,12 +10,14 @@ class Santa
     @reindeer_ranking =["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
+
   def celebrate_birthday
     @age += 1
   end
