ed7005b8f9b3ed93bb82188fe8f77adec2d399ad
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-07 15:59:21 +0200

    Add pseudocode for a reverse(string) method (7.1 - R4, step 2)

---
+++ js/explore.js
@@ -0,0 +1,16 @@
+
+//BUSINESS LOGIC
+
+// Method reverse
+// input: a string
+// output: the reversed string
+// START reverse
+//   create reversed_string, a new empty string
+//   FOR EACH char in string starting from the end
+//     add it to reversed_string
+//   return reversed_string
+// END reverse
+
+
+//TEST CODE
+//reverse("hello") == "olleh";
