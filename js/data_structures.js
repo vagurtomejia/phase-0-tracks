f1b3f075b8d7ee0a5e94be17b70408584a958e31
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-07 17:37:56 +0200

    Add a data_tructure.js file, and declare a colors and a names arrays (7.2 - RO, steps 2 to 4)

---
+++ js/data_structures.js
@@ -0,0 +1,15 @@
+
+//colors array declaration
+colors = ["snorkel blue", "iced coffee", "green flash", "peach echo"];
+
+//names array declaration
+names = ["Spirit", "Trojan", "Pegasus", "My Little Pony"];
+
+console.log(colors);
+console.log(names);
+
+colors.push("black");
+names.push("Tornado");
+
+console.log(colors);
+console.log(names);
