66cc98032449d4d47db020ad9645b308e5177909
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-07 16:11:47 +0200

    Add the reverse(string) function implementation (7.1 - R4, step 3)

--- js/explore.js
+++ js/explore.js
@@ -1,16 +1,21 @@
-
 //BUSINESS LOGIC
 
 // Method reverse
 // input: a string
 // output: the reversed string
 // START reverse
-//   create reversed_string, a new empty string
-//   FOR EACH char in string starting from the end
-//     add it to reversed_string
-//   return reversed_string
+function reverse(string) {
+  //create reversed_string, a new empty string
+  var reversed_string = '';
+  //FOR EACH char in string starting from the end
+  for (var i = string.length - 1; i >= 0; i--) {
+    //add it to reversed_string
+    reversed_string += string[i];
+    }
+  //return reversed_string
+  return reversed_string;
 // END reverse
-
+}
 
 //TEST CODE
-//reverse("hello") == "olleh";
+console.log(reverse("hello") == "olleh");
