6693bf3d99085e6dfd5c6a921538161ad5a46984
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-07 17:58:26 +0200

    Add a 'horses' object using the names and colors arrays (7.2 - R1, step 2)

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,11 +5,20 @@ colors = ["snorkel blue", "iced coffee", "green flash", "peach echo"];
 //names array declaration
 names = ["Spirit", "Trojan", "Pegasus", "My Little Pony"];
 
-console.log(colors);
-console.log(names);
-
+//add elements to colors and names arrays
 colors.push("black");
 names.push("Tornado");
 
-console.log(colors);
-console.log(names);
+//declare horses object
+var horses = {};
+
+//manage the case when the 2 arrays has a different length
+nbColors = colors.length;
+nbNames = names.length;
+nbIterations = nbColors >= nbNames ? nbNames : nbColors;
+
+//populate the object
+for (var i = 0; i < nbIterations; i++)
+  horses[names[i]] = colors[i];
+
+console.log(horses);
