05358bc7253db48d38a0bde4b3bf640710b49f19
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-09 16:46:00 +0200

    Add pseudocode for a findMatch function

--- js/algos.js
+++ js/algos.js
@@ -1,8 +1,7 @@
-
+//longestWord
 //input: phrasesArray, an array of words or phrases
 //output: the longest word in phrasesArray
-//example: phrasesArray = ["long phrase","longest phrase","longer phrase"]
-//         output = "longest phrase"
+//example: phrasesArray = ["long phrase","longest phrase","longer phrase"] => output = "longest phrase"
 
 function longestWord(phrasesArray) {
 
@@ -29,8 +28,30 @@ function longestWord(phrasesArray) {
     return phrasesArray[maxIndex];
 }
 
+//findMatch
+//inputs: object1, object2
+//outputs: true if the objects share at least one key-value pair. //         false if no pairs match
+//example: object1 = {name: "Steven", age: 54} and object2 = {name: "Tamir", age: 54} => output = true
+
+// declare a keyHasMatch variable
+// FOR EACH key1 in object1
+//   set keyHasMatch to false
+//   FOR EACH key2 in object2 OR UNTIL a match is found
+//     compare key1 and key2
+//     IF key1 equals key2
+//       compare value1 and value2
+//       IF value1 equals value2
+//         return true
+//       ELSE
+//         set keyHasMatch to true
+// return false
+
+
+
+
 
 //TEST CODE
 console.log(longestWord(["long phrase","longest phrase","longer phrase"]));
 
+//findMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54});
 
