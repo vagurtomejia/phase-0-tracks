740da7a27fc16b38ffd6d8e4941d4fd1fc6a1b6a
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-09 13:20:59 +0200

    Comment pseudocode for the Longest Word algorithm

--- js/algos.js
+++ js/algos.js
@@ -4,12 +4,12 @@
 //example: input = ["long phrase","longest phrase","longer phrase"],
 //          outputs = "longest phrase"
 
-declare an max_index variable and initialize it to -1
-declare a max_length variable and initialize it to 0
-FOR EACH phrase in the array
-  find the length of the phrase
-  IF the length is bigger than max_length
-    set max_index to the value of the index of the current phrase
-    set max_length to the value of the length  of the current phrase
+// declare an max_index variable and initialize it to -1
+// declare a max_length variable and initialize it to 0
+// FOR EACH phrase in the array
+//   find the length of the phrase
+//   IF the length is bigger than max_length
+//     set max_index to the value of the index of the current phrase
+//     set max_length to the value of the length  of the current phrase
 
-RETURN the phrase of phrases_array stocked at index number max_index
+// RETURN the phrase of phrases_array stocked at index number max_index
