5792dacc9672af99a64694938d85808a95d8e1a1
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-09 13:49:52 +0200

    Add longestWord function (7.3 - Release 0)

--- js/algos.js
+++ js/algos.js
@@ -1,15 +1,36 @@
 
-//input: phrases_array, an array of words or phrases
-//output: the longest word in phrases_array
-//example: input = ["long phrase","longest phrase","longer phrase"],
-//          outputs = "longest phrase"
+//input: phrasesArray, an array of words or phrases
+//output: the longest word in phrasesArray
+//example: phrasesArray = ["long phrase","longest phrase","longer phrase"]
+//         output = "longest phrase"
 
-// declare an max_index variable and initialize it to -1
-// declare a max_length variable and initialize it to 0
+function longestWord(phrasesArray) {
+
+// declare an maxIndex variable and initialize it to 0
+  var maxIndex = 0;
+// declare a maxLength variable and initialize it to 0
+  var maxLength = 0;
 // FOR EACH phrase in the array
-//   find the length of the phrase
-//   IF the length is bigger than max_length
-//     set max_index to the value of the index of the current phrase
-//     set max_length to the value of the length  of the current phrase
+  nbPhrases = phrasesArray.length;
 
-// RETURN the phrase of phrases_array stocked at index number max_index
+  for(var index = 0; index < nbPhrases; index++) {
+//    find the length of the phrase
+      phraseLength = phrasesArray[index].length;
+//    IF the length is bigger than max_length
+      if(phraseLength >= maxLength) {
+//      set maxIndex to the value of the index of the current phrase
+        maxIndex = index;
+//      set maxLength to the value of the length  of the current phrase
+        maxLength = phraseLength;
+      }
+  }
+
+  // RETURN the phrase of phrases_array stocked at index number max_index
+    return phrasesArray[maxIndex];
+}
+
+
+//TEST CODE
+console.log(longestWord(["long phrase","longest phrase","longer phrase"]));
+
+
