13983de4478f56934d9542b4d652c274036ee6a4
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-05-13 18:53:13 +0200

    Add non-detailed pseudocode as a description of the behavior of the application

---
+++ databases/polyglot_quotinator/polyglot_quotinator.rb
@@ -0,0 +1,38 @@
+
+#require needed libraries
+
+#BUSINESS LOGIC
+
+#create the database instance
+#set rows return type to hash
+
+#create the books table if not already exist
+
+#create the authors table if not already exist
+
+#create the quotes table if not already exist
+
+
+#USER INTERFACE
+
+#Present to the user the following possibilities:
+
+  #1-add a book
+  #2-add an author
+  #3-add a quote
+  #4-print a random quote
+
+#retrieve the input result
+
+#if the user input is 1
+  #add the book to the database
+
+#if the user input is 2
+  #add the author to the database
+
+#if the user input is 3
+  #add the quote to the database
+
+#if the user input is 4
+  #print a random quote from the database
+
