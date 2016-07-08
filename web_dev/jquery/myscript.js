cb0f0a313c9e1ce900356c9e574d99c9350f4327
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-07-08 14:46:28 -0500

    Create all the files for the Play with cotopus project

---
+++ web_dev/jquery/myscript.js
@@ -0,0 +1,7 @@
+
+//The jQuery's ready event runs code as soon as the document is ready to be manipulated (when the DOM is finished)
+$( document ).ready(function() {
+
+  console.log("This is my js script");
+
+});
