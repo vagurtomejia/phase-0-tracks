d67fb6fd018a4edd65654072bf6c0981c8285cc2
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-07-08 15:23:42 -0500

    Clean the project code

--- web_dev/jquery/myscript.js
+++ web_dev/jquery/myscript.js
@@ -2,6 +2,20 @@
 //The jQuery's ready event runs code as soon as the document is ready to be manipulated (when the DOM is finished)
 $( document ).ready(function() {
 
-  console.log("This is my js script");
+  $( "#btshow" ).click( function() {
+
+    var octopus = document.getElementById("octo_img");
+    $( octopus ).show( "slow" );
+
+  });
+
+
+  $( "#bthide" ).click( function() {
+
+    var octopus = document.getElementById("octo_img");
+    $( octopus ).hide( "slow" );
+
+  });
+
 
 });
