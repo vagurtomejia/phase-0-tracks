9911844f0547d23117ca5232cfff452fbd879622
vagurtomejia <veronica.agurto.mejia@gmail.com>
2016-07-10 19:18:17 -0500

    Add a route that displays an address (9.4, Releae 0, Q1)

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,10 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that displays an address
+
+get '/contact' do
+  "88w Unicorns Road"
+end
+
